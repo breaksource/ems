871c189a77f120a18747e9f0569d4a36ec1bc708
zhangpan <half-beast@163.com>
2016-09-04 21:21:52 +0800

    工单

--- app/src/main/java/com/example/ems/WorkOrderActivity.java
+++ app/src/main/java/com/example/ems/WorkOrderActivity.java
@@ -2,6 +2,9 @@ package com.example.ems;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.ems.view.BackArrow;
 
 /**
  * Created by zhangpan on 16/9/4.
@@ -12,9 +15,17 @@ public class WorkOrderActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workorder);
+
+        init();
     }
 
     private void init() {
-
+        BackArrow backArrow = (BackArrow) findViewById(R.id.back);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
