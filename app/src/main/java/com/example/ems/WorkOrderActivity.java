f847fd0abce2df30499719ddc853644007d39737
zhangpan <half-beast@163.com>
2016-09-04 20:18:58 +0800

    工单界面

---
+++ app/src/main/java/com/example/ems/WorkOrderActivity.java
@@ -0,0 +1,20 @@
+package com.example.ems;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhangpan on 16/9/4.
+ */
+public class WorkOrderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    private void init() {
+
+    }
+}
