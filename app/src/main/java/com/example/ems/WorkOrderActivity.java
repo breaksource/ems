2c61acbf22f0e53858eb53e8c98a150829a792a2
zhangpan <half-beast@163.com>
2016-09-04 21:11:56 +0800

    工单

--- app/src/main/java/com/example/ems/WorkOrderActivity.java
+++ app/src/main/java/com/example/ems/WorkOrderActivity.java
@@ -11,7 +11,7 @@ public class WorkOrderActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_workorder);
     }
 
     private void init() {
