ef7b26f08db4f919865226f091322290f7a7efad
zhangpan <half-beast@163.com>
2016-09-04 21:32:33 +0800

    详细

---
+++ app/src/main/java/com/example/ems/DetailActivity.java
@@ -0,0 +1,21 @@
+package com.example.ems;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.ems.view.BackArrow;
+
+/**
+ * Created by zhangpan on 16/9/4.
+ */
+public class DetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workorder);
+
+
+    }
+}
