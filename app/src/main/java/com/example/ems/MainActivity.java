2dd9a7c6de90ccbe4e5934240fe339c3f1c8c3e2
zhangpan <half-beast@163.com>
2016-09-04 20:56:12 +0800

    工单录入

--- app/src/main/java/com/example/ems/MainActivity.java
+++ app/src/main/java/com/example/ems/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ems;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login();
+    }
+
+    private void login() {
+        TextView textView = (TextView) findViewById(R.id.login);
+        textView.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,WorkOrderActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
